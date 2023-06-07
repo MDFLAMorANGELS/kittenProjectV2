806d9762c63a5520e9e0346dfafbf99812de1935
caroline <gensac.caroline@gmail.com>
2023-06-07 23:38:31 +0200

    show cart + bouton mon panier

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,9 @@
 class CartsController < ApplicationController
   def show
+    @items_in_cart = JoinTableItemsCart.where(cart_id:current_user.cart.id)
   end
+  #trouves-moi les entrées où le panier utilisé est le même que celui de la personne connecté puis enregistre tout ça dans la variable « @items_in_cart ». 
+  #
 
   def create
   end
